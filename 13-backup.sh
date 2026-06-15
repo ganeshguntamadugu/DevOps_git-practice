e145700b92cf605aa1afcbc947166c1d8dc46bbe
Ganesh Guntamadugu <ganeshgana868@gmail.com>
2026-06-15 14:30:15 +0530

    Updated something in ShellScript-practice

--- 13-backup.sh
+++ 13-backup.sh
@@ -25,7 +25,7 @@ then
     exit 1
 fi
 
-mkdir -p $Destination
+mkdir -p "$Destination"
 
 if [ ! -d $Destination ]
 then
@@ -35,11 +35,19 @@ fi
 
 Files=$(find $Source -name "*.log" -mtime +$Days)
 
-if [ -z "$Files" ]
+if [ -z "$Files" ] # It will shows the empty files
 then 
     echo -e "Files$R doesn't$N exits more than $Days in $Source"
+    exit 1
 else
     echo -e "Files are$G exits$N more than $Days in $Source"
-    echo "$Files"
+    # echo "$Files"
+    $Files | zip logs-$(date +"%B,%d,%Y-%T") -@  
+    echo ""
+    while IFS= read -r gana #IFS, internal field seperator, empty it will ignore while space, -r is for not to ignore special charecters like /
+    do
+        echo "Deleting file $gana"
+        rm -rf $gana
+    done <<< $Files 
 fi
 
